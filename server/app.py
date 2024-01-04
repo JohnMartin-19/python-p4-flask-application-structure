74e2a18f38ca65b958832076a7f3b761bde67046
John <johnnkonge2020@gmail.com>
2024-01-04 09:37:04 +0300

    added a dev server as a module

--- server/app.py
+++ server/app.py
@@ -3,6 +3,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(port=5555)
 @app.route('/<string:username>')
 def user(username):
     return f'<h1>Profile for {username}</h1>'
